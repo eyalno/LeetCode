e3b7cb2aaae784c00e6a20a0b6b618175529f348
Eyal Noy <eyalnoy@gmail.com>
2025-02-16 13:17:54 +0200

    Grind169

---
+++ Grind169.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <unordered_set>
+#include <set>
+#include <utility>
+#include <bitset>
+#include <stack>
+#include <array>
+#include <sstream>
+#include <iomanip>
+
+#include "lib/TreeNode.h"
+#include "lib/ListNode.h"
+#include "lib/Trie.h"
+#include "lib/DisJointSet.h"
+#include "lib/LinkedList.h"
+using namespace std;
+//ordered by topics
+//1. Two Sum.  Unsorted
+
+vector<int> twoSumUnsorted(vector<int>& nums, int target) {
+
+unordered_map<int,int> hash;
+
+
+for (int i = 0; i < nums.size(); i++){
+
+    int comp = target - nums[i];
+
+ 
+
+}
+
+return {};
+
+}
