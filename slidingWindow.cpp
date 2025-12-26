fd9c99c47cb801b0c08c94c894fb3d6dfb511388
Eyal Noy <eyalnoy@gmail.com>
2025-12-26 09:18:15 -0600

    Sliding window

--- slidingWindow.cpp
+++ slidingWindow.cpp
@@ -18,3 +18,16 @@
 #include "lib/Trie.h"
 #include "lib/DisJointSet.h"
 #include "lib/LinkedList.h"
+
+
+
+//https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/description/
+
+//1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+
+//    Equivalent condition:
+//      max(subarray) - min(subarray) < N
+
+int longestSubarray(vector<int>& nums, int limit) {
+        
+}
